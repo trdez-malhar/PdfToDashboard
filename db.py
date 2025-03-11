a1754bbed7c8c4dd952acb5c5b3eca95ab10d34d
malhar.yadav <malhar.yadav@trdez.com>
2025-03-11 10:48:08 +0530

    update

--- db.py
+++ db.py
@@ -30,17 +30,28 @@ def insert_data_bulk(table_name, records):
 
         # Construct SQL query
         sql = text(f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})")
-        # print(f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})")
+        print(f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})")
         # Execute bulk insert
-        result = session.execute(sql, records)  # `records` is a list of dictionaries
+        # result = session.execute(sql, records)  # `records` is a list of dictionaries
         # print("call")
-        session.commit()  # Commit transaction
+        # session.commit()  # Commit transaction
 
-        print(f"{result.rowcount} records inserted successfully!")
-        return result.rowcount  # Return number of affected rows
+        # print(f"{result.rowcount} records inserted successfully!")
+        # return result.rowcount  # Return number of affected rows
     except Exception as e:
         print(f"Error: {e}")
         session.rollback()  # Rollback if error occurs
         return None
     finally:
         session.close()
+
+def read_predefined_data():
+    import json
+    with open(r"C:\Users\malhar.yadav\scripts\PdfToDashboard\final_data_MSY.json", mode="r", encoding="utf-8") as fp:
+        pdata = json.loads(fp.read())
+    return pdata
+# print(read_predefined_data())
+
+def add_data():
+    pdata = read_predefined_data()
+
